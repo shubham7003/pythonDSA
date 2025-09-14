527602d7972a376b531f1e1d2480efba1b7b6d82
Shubham Kumar Dubey <152261769+shubham7003@users.noreply.github.com>
2025-09-14 20:33:27 +0000

    error fix in insertion

--- Sorting/insertionSort.py
+++ Sorting/insertionSort.py
@@ -5,7 +5,10 @@ def insertionSort(arr):
         while j>=0 and key < arr[j]:   #shifiting elements that are greater than key to one position ahead of their current position
             arr[j+1] = arr[j]  #shifting element to one position ahead
             j -= 1  # decrementing j for ending while loop and comparing with next
-        arr[j+1] = #key placing key after the element just smaller than it.  
+        arr[j+1] = key #key placing key after the element just smaller than it.  
 
     
     return arr
+
+a = [12,11,13,5,6]
+print(insertionSort(a))
